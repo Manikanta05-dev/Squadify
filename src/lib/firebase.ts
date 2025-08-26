be5b52ecb7e02216930560a9d547f97d7db0653f
Mani kanta <bhupamanikanta05@gmail.com>
2025-08-26 09:24:15 +0000

    firebase erreor auth/configuration not found

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
@@ -15,12 +15,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
